4d5f59e7e001b6bb88872c04b3b09d3e345830a1
ComputationalDomain <110780995+ComputationalDomain@users.noreply.github.com>
2023-02-19 17:34:54 +0100

    Created NS_PINNS

---
+++ NS_PINNS.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import scipy.io
+from torch.autograd import Variable
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
